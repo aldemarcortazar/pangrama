adec083c68b46301584e389e9284866b10e1d314
aldemar niño cortazar <anino79@misena.edu.co>
2021-04-05 17:10:19 -0500

    Agregar archivos de proyecto.

---
+++ Pangrama.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pangrama
+{
+    class Pangrama
+    {
+
+        public List<char> abecedario()
+        {
+            List<char> pangrama = new List<char>()
+            {
+                'a',
+                'b',
+                'c',
+                'd',
+                'e',
+                'f',
+                'g',
+                'h',
+                'i',
+                'j',
+                'k',
+                'l',
+                'm',
+                'n',
+                'ñ',
+                'o',
+                'p',
+                'q',
+                'r',
+                's',
+                't',
+                'u',
+                'v',
+                'w',
+                'x',
+                'y',
+                'z'
+             };
+
+            return pangrama;
+        }
+
+        public void compararPagrama(string frase)
+        {
+            List<char> alfabeto = abecedario();
+            List<char> letrasContains = new List<char>();
+            for(int i = 0; i < frase.Length; i++  )
+            {
+                if ( alfabeto.Contains(frase[i]) && !letrasContains.Contains(frase[i]))
+                {
+                    letrasContains.Add( frase[i] );
+                }
+            }
+
+
+            if( alfabeto.Count == letrasContains.Count)
+            {
+                Console.WriteLine("la palabra es un pangrama");
+            }
+            else
+            {
+                List<char> caracteresNocontains = new List<char>();
+                for (int i = 0; i < alfabeto.Count; i++)
+                {
+                    if(!letrasContains.Contains(alfabeto[i]))
+                    {
+                        caracteresNocontains.Add(alfabeto[i]);   
+                    }
+                }
+
+                Console.WriteLine("las letras que faltan para que esto sea un pangrama son: " + string.Join("," , caracteresNocontains) );
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace pangrama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! seguidamente se te solicitaran que escribas una palabra para comparar si es un pangrama");
+            string frase = Console.ReadLine().ToLower();
+            Pangrama pangrama = new Pangrama();
+            pangrama.compararPagrama( frase );
+            Console.ReadKey();
+        }
+    }
+}
